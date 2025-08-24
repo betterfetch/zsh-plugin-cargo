3b4b1cee43dba4d98b4ecdfff3540144f2f76a36
David Balishyan <davidbalishyan12@gmail.com>
2025-08-24 12:46:28 +0400

    initial commit. Added: Aliases

---
+++ zsh-plugin-cargo.plugin.zsh
@@ -0,0 +1,14 @@
+# Functions
+rust-tools-verisons (){
+  cargo --version
+  rustc --verstion
+  rustup --version
+}
+
+# Alias
+alias c="cargo"
+alias cr="cargo run"
+alias ccr="cargo clean && cargo run"
+alias ccl="cargo clean"
+alias ci="cargo install"
+alias ca="cargo add"
